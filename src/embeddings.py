cee024f4895aa7fea42eda0d76e68b47ac9f2d5a
MaelTriquet <mael1triquet@gmail.com>
2025-03-27 14:42:36 +0100

    batch vectorising articles

--- src/embeddings.py
+++ src/embeddings.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 import json
 import re
+import os
 
 
 model = spacy.load('en_core_web_md')
@@ -29,7 +30,7 @@ def compute_article_embeddings(article_json, nlp):
             doc = nlp(article_text)
             embeddings.append(doc.vector)
     print(len(embeddings))
-    return np.array(embeddings)
+    return embeddings
 
 def store_question_vectors_in_h5(filename, vectors, dataset_name='vectors'):
     vectors_array = np.array(vectors)
@@ -42,9 +43,9 @@ def store_question_vectors_in_h5(filename, vectors, dataset_name='vectors'):
 
     print(f"Vectors successfully stored in {filename}")
     
-def store_article_vectors_in_h5(filename, vectors, dataset_name='vectors'):
+def store_article_vectors_in_h5(filename, vectors, contents, dataset_name='vectors'):
     vectors_array = np.array(vectors)
-    contents_array = np.array([(re.sub(r'\[.*?\]', '', i['title']) + " | " + j["id"]).encode("utf-8") for i in contents for j in i["content"]])
+    contents_array = np.array(contents)
     
     # i = -1
     # for string in contents_array:
@@ -52,12 +53,17 @@ def store_article_vectors_in_h5(filename, vectors, dataset_name='vectors'):
     #     new_string = re.sub(r'\[.*?\]', '', string)
     #     contents_array[i] = new_string.encode('utf-8')
     # Create an .h5 file and store the vectors
-    with h5py.File(filename, 'w') as h5file:
+    with h5py.File(filename, 'a') as h5file:
         h5file.create_dataset("text", data=contents_array)
         h5file.create_dataset(dataset_name, data=vectors_array)
 
     print(f"Vectors successfully stored in {filename}")
     
+def handle_article_file(filename, contents, embeddings):
+    with open(filename, 'r') as file:
+        articles_json = json.load(file)
+    contents += [(re.sub(r'\[.*?\]', '', i['title']) + " | " + j["id"]).encode("utf-8") for i in articles_json for j in i["content"]]
+    embeddings += compute_article_embeddings(articles_json, model)
 
 embed = "articles"
 if embed == "questions":
@@ -69,9 +75,16 @@ if embed == "questions":
     question_embeddings = compute_question_embeddings(questions_json, model)
     store_question_vectors_in_h5("question_embeddings.h5", question_embeddings)
 elif embed == "articles":
-    with open('formated/EPC/formated_articles.json', 'r') as file:
-        articles_json = json.load(file)
-        
-    contents = articles_json
-    article_embeddings = compute_article_embeddings(articles_json, model)
-    store_article_vectors_in_h5("article_embeddings.h5", article_embeddings)
+    embeddings = []
+    contents = []
+    directory_path = '/home/mael/Documents/Data_Battle/crispy-potato/data/official_legal_publications'
+    
+    for root, dirs, files in os.walk(directory_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            handle_article_file(file_path, contents, embeddings)
+            print("handled file : ", file_path)
+
+    
+    # handle_article_file("../data/official_legal_publications/EPC/formated_articles.json", contents, embeddings)
+    store_article_vectors_in_h5("../bin/article_embeddings.h5", embeddings, contents)
--- todo.md
+++ todo.md
@@ -1,3 +1,2 @@
 # store the article the question was generated from?
-# vectorise more data
 # finally use the exams to generate questions
